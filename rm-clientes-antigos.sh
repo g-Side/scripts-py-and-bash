24af01bafbd28abd866a1b90179c6bdb496ec710
Jean Alencar <jean.alencar@hotmail.com>
2025-11-11 16:32:13 -0300

    resolvendo problema de lógica

--- rm-clientes-antigos.sh
+++ rm-clientes-antigos.sh
@@ -4,7 +4,7 @@
 #LINK DO DISCORD 
 WEBHOOK_URL="https://discord.com/api/webhooks/1407444877905494177/Qm3JIvdeZGLKjZbolLd1vP8wzRU6KX0kroeC5axqmZ9tVO0ExhPNAn9ExqL5H_JuyS-s"
 #PASTA PARA REMOVER
-CLIENTES_EXCLUIR=$(ls -la /var/www | grep "EXCLUIR*")
+CLIENTES_EXCLUIR=($(ls -d /var/www/*EXCLUIR*/ | xargs -n 1 basename))
 #FORMATANDO DATA
 DATE=$(date +'%Y/%m/%d-%H:%M')
 #DEFININDO ARQUIVO DE LOG E CRIANDO. CASO JÁ EXISTA NÃO FAZ NADA
@@ -23,13 +23,15 @@ LOG_APLICACAO() {
     echo "EM $DATE o $cliente foi removido." >> $ARQUIVO_LOG
     }
 
-for cliente in CLIENTES_EXCLUIR ; do
-    if [[] -n "$cliente" ]]; then
+for cliente in "{CLIENTES_EXCLUIR[@]}" ; do
+    if [[ -n "$cliente" ]]; then
         echo "Encontrado o cliente $cliente para excluir"
         #########COMENTADO PARA TESTE
         ##rm -Rf $cliente
         LOG_APLICACAO
-        MSG=":rotating_light: $cliente foi removido do servidor PRO automaticamente em: $DATE"
+        ###USANDO OUTRA MENSAGEM DE TESTE PRO DISCORD
+        ###MSG=":rotating_light: $cliente foi removido do servidor PRO automaticamente em: $DATE"
+        MSG=":wrench::construction::construction_worker: Mensagem de teste, apenas teste.:wrench::construction::construction_worker:"
         NOTIFICA_DISCORD
     fi
 done

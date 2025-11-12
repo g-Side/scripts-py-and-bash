78d99be4cfd46c3051d9f5207bc0d476d5a0b4bc
Jean Alencar <jean.alencar@hotmail.com>
2025-11-12 10:01:55 -0300

    comentando parte do log pra testar

--- excluir_clientes_desativados.py
+++ excluir_clientes_desativados.py
@@ -3,12 +3,6 @@ import datetime as dt
 from time import sleep as sp 
 
 """
-1.DEFINIR PASTA ONDE ELE VAI TRABALHAR - OK
-2.FAZER A BUSCA E FILTRO DAS PASTAS QUE CONTENHAM "EXCLUIR_" NO NOME
-3.ADICIONAR NUMA LISTA ESSES NOMES DE PASTAS
-5.ADICIONAR NUM ARQUIVO DE LOG PARA ARMAZENAR OS NOMES DOS ITENS EXCLUIDOS
-
-4.REMOVER OS ITENS
 6.REMOVER DO SITES-ENABLE E DAR UM REFRESH NO APACHE2
 """
 
@@ -20,14 +14,14 @@ pasta_clientes = '/var/www/'
 """LISTA DOS ITENS PARA EXCLUIR"""
 filtrados_excluir = []
 
-"""ARQUIVO DE LOG:"""
-arquivo_log = '/var/log/clientes_excluidos.txt'
-with open(arquivo_log, 'a') as log:
-    log.write('Data:')
-
 """CONFIGURANDO A DATA"""
 data_atual = dt.datetime.now()
-date = data_atual.strftime("%Y-%m-%d")
+data = data_atual.strftime("%Y-%m-%d")
+
+"""ARQUIVO DE LOG:
+arquivo_log = '/var/log/clientes_excluidos.txt'
+with open(arquivo_log , 'a') as log:
+    log.write(f'Data: {data}')"""
     
 def limpar_clientes_antigos():
     try:
@@ -40,9 +34,9 @@ def limpar_clientes_antigos():
             if i.startswith('EXCLUIR_'):
                 filtrados_excluir.append(i)
                 """ultimo_item = filtrados_excluir[-1]"""
-                print(f'{date} - {filtrados_excluir[-1]} foi excluido.')
-                """print(f'{date} - {ultimo_item} foi excluido.')"""
-                """log.write(f'{date} - {ultimo_item} foi excluido.')"""
+                print(f'{data} - {filtrados_excluir[-1]} foi excluido.')
+                """print(f'{data} - {ultimo_item} foi excluido.')"""
+                """log.write(f'{data} - {ultimo_item} foi excluido.')"""
     except FileNotFoundError:
         print(f'Erro: o diretório não foi encontrado')
 limpar_clientes_antigos()

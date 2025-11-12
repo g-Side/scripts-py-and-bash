634c7e123963e0ed733b279aad090be292c2b25e
Jean Alencar <jean.alencar@hotmail.com>
2025-11-12 16:52:32 -0300

    teste de log

--- excluir_clientes_desativados.py
+++ excluir_clientes_desativados.py
@@ -27,8 +27,9 @@ def limpar_clientes_antigos():
                 filtrados_excluir.append(i)
                 ultimo_item = filtrados_excluir[-1]
                 print(ultimo_item)
+                teste = f"{data} teste log. \n"
                 with open('arquivo_log' , 'a') as log:
-                    log.write("data teste log.")
+                    log.write(teste)
                 # shutil.rmtree(ultimo_item)
                 # print(f'{ultimo_item} foi removido')
 

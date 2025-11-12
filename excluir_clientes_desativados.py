7040da2ad85c1d348dd28f2ec95473af5ff8ec99
Jean Alencar <jean.alencar@hotmail.com>
2025-11-12 15:48:12 -0300

    teste de log

--- excluir_clientes_desativados.py
+++ excluir_clientes_desativados.py
@@ -3,12 +3,12 @@ import datetime as dt
 from time import sleep as sp 
 import shutil
 
-
-# 6. REMOVER DO SITES-ENABLE E DAR UM REFRESH NO APACHE2
-
 # DECLARAÇÃO DAS VARIAVEIS
 # --- PASTA DE TRABALHO DO SCRIPT:
 pasta_clientes = '/var/www/'
+
+# ARQUIVO DE LOG (Comentado):
+arquivo_log = "/var/log/clientes_excluidos.txt"
 
 # LISTA DOS ITENS PARA EXCLUIR
 filtrados_excluir = []
@@ -28,10 +28,8 @@ def limpar_clientes_antigos():
                 filtrados_excluir.append(i)
                 ultimo_item = filtrados_excluir[-1]
                 print(ultimo_item)
-                # ARQUIVO DE LOG (Comentado):
-                arquivo_log = "/var/log/clientes_excluidos.txt"
-                with open(arquivo_log , 'a') as log:
-                    log.write(f'{data} teste log.\n')
+                # with open(arquivo_log , 'a') as log:
+                    # log.write(f'{data} teste log.\n')
                 # shutil.rmtree(ultimo_item)
                 # print(f'{ultimo_item} foi removido')
 

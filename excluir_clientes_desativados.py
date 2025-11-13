61e179473696ad48f0cb5a8fc2ae7afb09724395
Jean Alencar <jean.alencar@hotmail.com>
2025-11-13 10:25:32 -0300

    v1 funcionando - foi testado localmente

--- excluir_clientes_desativados.py
+++ excluir_clientes_desativados.py
@@ -4,17 +4,17 @@ import shutil
 
 # DECLARAÇÃO DAS VARIAVEIS
 # --- PASTA DE TRABALHO DO SCRIPT:
-pasta_clientes = '/var/www/'
-
-# ARQUIVO DE LOG (Comentado):
-arquivo_log = '/var/log/clientes_excluidos.txt'
-
-# LISTA DOS ITENS PARA EXCLUIR
-filtrados_excluir = []
+pasta_clientes = '/home/jean/teste-script/'
 
 # CONFIGURANDO A DATA
 data_atual = dt.datetime.now()
 data = data_atual.strftime("%Y-%m-%d")
+
+# ARQUIVO DE LOG (Comentado):
+arquivo_log = '/home/jean/teste-script/clientes_excluidos.txt'
+
+# LISTA DOS ITENS PARA EXCLUIRk
+filtrados_excluir = []
 
 def limpar_clientes_antigos():
     try:
@@ -25,16 +25,15 @@ def limpar_clientes_antigos():
         for i in itens:
             if i.startswith('EXCLUIR_'):
                 filtrados_excluir.append(i)
-                ultimo_item = filtrados_excluir[-1]
+                ultimo_item = filtrados_excluir[-1]+'/'
                 print(ultimo_item)
-                teste = data +  'teste log. \n'
+                path_completo = pasta_clientes+ultimo_item
+                shutil.rmtree(path_completo)
+                print(f'{ultimo_item} foi removido')
+                #GERENCIAMENTO DO LOG
+                logar = data + ultimo_item + ' foi removido \n'
                 with open(arquivo_log , 'a') as log:
-                    log.write(teste)
-                with open (arquivo_log) as f:
-                    print(f.read())
-                # shutil.rmtree(ultimo_item)
-                # print(f'{ultimo_item} foi removido')
+                    log.write(logar)
     except FileNotFoundError:
         print('Erro: o diretório não foi encontrado')
-        
 limpar_clientes_antigos()

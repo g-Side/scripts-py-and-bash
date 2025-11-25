2ad1cf5e6f13860a97af68591dc8c03ab393b41d
Jean Alencar <jean.alencar@hotmail.com>
2025-11-25 16:44:39 -0300

    melhorando o script

--- notificacao-acessos-prod.sh
+++ notificacao-acessos-prod.sh
@@ -5,8 +5,8 @@
 WEBHOOK_URL="https://discordapp.com/api/webhooks/1442874750811766804/7CY6soEMp_fTX0JMIa1UWpMcP0aOilb3LeMSBZI28NGKKLicB5xmeXujvZzu8ozmlgKm"
 
 ### FUNCAO PARA PEGAR O RESULTADO DAS CONTAGENS DE ACESSOS
-REQ=$(netstat -tulnp | grep "80" | wc -l)
-MSG=$(echo "Quantidade de requisicoes agora: $REQ")
+REQ=$(netstat -atn | grep ":80" | wc -l)
+MSG=$(echo "Quantidade de requisicoes server: $REQ")
 
 ### FUNCAO PARA ENVIAR A MENSAGEM AO DISCORD
 NOTIFICA_DISCORD() {
@@ -15,5 +15,4 @@ NOTIFICA_DISCORD() {
        -d "{\"content\": \"$MSG\"}" \
        $WEBHOOK_URL
        }
-
 NOTIFICA_DISCORD

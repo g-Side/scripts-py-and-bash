cd10950e93f1ef24a3c1ae84dd8a2d9f1c9b7816
Jean Alencar <jean.alencar@hotmail.com>
2025-11-26 10:06:22 -0300

    alterando script para teste

--- mover-imagens.py
+++ mover-imagens.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from time import sleep
 
 '''Caminho para buscar a planilha'''
-planilha = "/var/www/negreiros/imageUpload-bkp/scripts-py-and-bash/mover-imagens.py"
+planilha = "/var/www/negreiros/imageUpload-bkp/scripts-py-and-bash/cf_imagem.csv"
 cliente_midia = Path('/var/www/negreiros/imageUpload-bkp/')
 
 '''Verificar se a pasta existe'''
@@ -12,21 +12,22 @@ if not cliente_midia.is_dir():
     print('Cliente não encontrado.')
     exit
 else:
-    print('Cliente selecionado.')
+    print('Pasta de backup selecionada.')
     os.system('mkdir imagens-movidas')
 
 '''Importar a planilha pro programa'''
 data = pd.read_csv(planilha)
 if not data.empty:
-    print('Importando dados para a remoção.')
-'''Busca pela coluna que eu quero'''
-imagens = data['imagens_del']
-print('Dados selecionados.')
+    print('Planilha vazia. Verifique o conteudo')
+    exit
+else:
+    imagens = data['imagens_del']
+    print('Dados carregados.')
 '''Função de apagar as imagens'''
 def apagar_imagens():
     try: 
         for i in imagens:
-            os.system('mv {} imagens-movidas/'.format(i))
+            #os.system('mv {} imagens-movidas/'.format(i))
             print('o item {} movido'.format(i))
     except:
         print('Erro ao executar script.')

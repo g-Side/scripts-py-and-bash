c22da6b93697ece905fc18a5d9c801b6976dad0d
root <root@ip-172-31-47-176.us-east-2.compute.internal>
2025-11-27 14:36:43 -0300

    script funcional em homologação

--- mover-imagens.py
+++ mover-imagens.py
@@ -1,11 +1,12 @@
 import pandas as pd
-import os
+import shutil, os
 from pathlib import Path
 from time import sleep
 
 '''Caminho para buscar a planilha'''
-planilha = "/var/www/negreiros/imageUpload-bkp/scripts-py-and-bash/cf_imagem.csv"
-cliente_midia = Path('/var/www/negreiros/imageUpload-bkp/')
+planilha = "/home/jean.alencar/scripts-py-and-bash/cf_imagem.csv"
+cliente_midia = Path('/var/www/negreiros/imageUpload/')
+imagens_movidas = Path('/home/jean.alencar/scripts-py-and-bash/imagens-movidas/')
 
 '''Verificar se a pasta existe'''
 if not cliente_midia.is_dir():
@@ -13,22 +14,30 @@ if not cliente_midia.is_dir():
     exit()
 else:
     print('Pasta de backup selecionada.')
-    os.system('mkdir imagens-movidas')
+    if not imagens_movidas.is_dir():
+        os.system(f'mkdir {imagens_movidas}')
 
+print('Pasta de destino encontrada.')
 '''Importar a planilha pro programa'''
 data = pd.read_csv(planilha)
-if not data.empty:
-    print('Planilha vazia. Verifique o conteudo')
-    exit()
-else:
-    imagens = data['imagens_del']
-    print('Dados carregados.')
+print('Planilha encontrada.')
+imagens = data['imagens_del']
+print('Dados carregados.')
 '''Função de apagar as imagens'''
 def apagar_imagens():
     try: 
         for i in imagens:
-            #os.system('mv {} imagens-movidas/'.format(i))
-            print('o item: {} foi encontrado e será movido.'.format(i))
-    except:
-        print('Erro ao executar script.')
+            arquivo_origem = f'{cliente_midia}/{i}'
+            arquivo_path = Path(arquivo_origem)
+            if not arquivo_path.is_file():
+                print(f'Arquivo {i} não existe na pasta.')
+                continue
+            shutil.move(arquivo_origem, imagens_movidas)
+            print(f'Movido de: {arquivo_origem} para: {imagens_movidas}.')
+    except FileNotFoundError:
+        print('Arquivo não encontrado.')
+    except PermissionError:
+        print('Erro de execução por falta de permissão. Você é root?')
+'''    except:
+        print('Erro genérico. Por favor, contate o desenvolvedor do script.')'''
 apagar_imagens()
